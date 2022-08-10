1b357ef417da20dfaf3ab19879773fd877011795
JuniorSO <rogersojr@gmail.com>
2022-08-10 17:45:23 -0300

    add: map creation with ping

---
+++ app/src/main/java/com/example/inciforma/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.inciforma
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(-23.522191184043685, -46.47568209691291))
+                .title("ETEC Zona Leste")
+        )
+    }
+}
